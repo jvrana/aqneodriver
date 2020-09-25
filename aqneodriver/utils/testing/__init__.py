d7eee3b88c233bc6825e9e3965387b65363850ac
Justin Vrana <justin.vrana@gmail.com>
2020-09-25 09:06:46 -0700

    tests

---
+++ aqneodriver/utils/testing/__init__.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from typing import Any
+import subprocess
+
+
+def cmd_output(*cmd: str, retcode: Optional[int] = 0, **kwargs: Any) -> str:
+    kwargs.setdefault("stdout", subprocess.PIPE)
+    kwargs.setdefault("stderr", subprocess.PIPE)
+    proc = subprocess.Popen(cmd, **kwargs)
+    stdout, stderr = proc.communicate()
+    stdout = stdout.decode()
+    if retcode is not None and proc.returncode != retcode:
+        raise subprocess.CalledProcessError(cmd, retcode, proc.returncode, stdout, stderr)
+    return stdout
